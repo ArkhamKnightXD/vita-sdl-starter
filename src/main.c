0f088994c00e1a76aaa1bce3d53491fd8eba5206
knight <karvin.jimenez@alternard.com>
2024-05-20 17:53:46 -0400

    changing code style.

--- src/main.c
+++ src/main.c
@@ -7,31 +7,31 @@ enum {
   SCREEN_HEIGHT = 544
 };
 
-SDL_Window  *gWindow = NULL;
-SDL_Renderer *gRenderer = NULL;
+SDL_Window  *window = NULL;
+SDL_Renderer *renderer = NULL;
 
 SDL_Rect fillRect = {SCREEN_WIDTH  / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH  / 2, SCREEN_HEIGHT / 2};
 
 int main(int argc, char *argv[])
 {
-  if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+  if(SDL_Init( SDL_INIT_VIDEO ) < 0)
       return -1;
 
-  if ((gWindow = SDL_CreateWindow( "RedRectangle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN)) == NULL)
+  if ((window = SDL_CreateWindow( "RedRectangle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN)) == NULL)
     return -1;
 
-  if ((gRenderer = SDL_CreateRenderer( gWindow, -1, 0)) == NULL)
+  if ((renderer = SDL_CreateRenderer(window, -1, 0)) == NULL)
       return -1;
 
-  SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 255);
-  SDL_RenderFillRect(gRenderer, &fillRect);
-  SDL_RenderPresent(gRenderer);
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderFillRect(renderer, &fillRect);
+  SDL_RenderPresent(renderer);
   SDL_Delay(4000);
-  SDL_DestroyRenderer(gRenderer);
-  SDL_DestroyWindow(gWindow);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
 
-  gWindow = NULL;
-  gRenderer = NULL;
+  window = NULL;
+  renderer = NULL;
   SDL_Quit();
   return 0;
 }
